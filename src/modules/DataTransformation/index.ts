dcb4207393aa9920fd741d1de4b6e3d90823b52c
LRemp <l.lbruzga.b@gmail.com>
2024-06-09 13:07:04 +0300

    feat: create base DataTransformation class

---
+++ src/modules/DataTransformation/index.ts
@@ -0,0 +1,11 @@
+interface IDataTransformation {}
+
+class DataTransformation implements IExecutable, IDataTransformation {
+  constructor() {}
+
+  public execute() {
+    //TODO: implement data transformation entry point
+  }
+}
+
+export default DataTransformation;
---
+++ src/types/executable.d.ts
@@ -0,0 +1,3 @@
+interface IExecutable {
+  execute: () => void;
+}
