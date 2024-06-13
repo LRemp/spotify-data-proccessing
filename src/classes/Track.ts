4787b293ee5ffc20249d6e1a7d043d9a5f488330
LRemp <l.lbruzga.b@gmail.com>
2024-06-13 10:45:20 +0300

    refactor: split parts based on regex expression

--- src/classes/Track.ts
+++ src/classes/Track.ts
@@ -51,15 +51,26 @@ export default class Track implements IParsable {
   }
 
   public parse(data: string) {
-    const parts: string[] = data.split(",");
+    const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
+    const parts: string[] = data.split(regex);
 
     this.id = parts[0];
     this.name = parts[1];
     this.popularity = parseInt(parts[2]);
     this.duration_ms = parseInt(parts[3]);
     this.explicit = parseInt(parts[4]);
-    this.artists = JSON.parse(parts[5].replace(/'/g, '"'));
-    this.id_artists = JSON.parse(parts[6].replace(/'/g, '"'));
+    this.artists = JSON.parse(
+      parts[5]
+        .trim()
+        .replace(/^"+|"+$/g, "")
+        .replace(/'/g, '"')
+    );
+    this.id_artists = JSON.parse(
+      parts[6]
+        .trim()
+        .replace(/^"+|"+$/g, "")
+        .replace(/'/g, '"')
+    );
     this.release_date = parts[7];
     this.danceability = parseFloat(parts[8]);
     this.energy = parseInt(parts[9]);
