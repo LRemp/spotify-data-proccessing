350118d70bec92e741a1f2de84e9e3a2de25b78a
LRemp <l.lbruzga.b@gmail.com>
2024-06-14 10:09:02 +0300

    feat: create track release date explode

---
+++ src/modules/DataTransformation/explosions/ExplodeTrackReleaseDate.ts
@@ -0,0 +1,7 @@
+import Track from "../../../classes/Track";
+
+export default function ExplodeTrackReleaseDate(data: Track) {
+  data.year = data.release_date.getFullYear();
+  data.month = data.release_date.getMonth() + 1;
+  data.day = data.release_date.getDate();
+}
