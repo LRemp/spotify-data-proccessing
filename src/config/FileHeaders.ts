c991414981333ab3b284cb5780ee1fd491bccb27
LRemp <l.lbruzga.b@gmail.com>
2024-06-14 10:08:25 +0300

    feat: create constants for file headers

---
+++ src/config/FileHeaders.ts
@@ -0,0 +1,7 @@
+export default {
+  trackFileHeader:
+    "id,name,popularity,duration_ms,explicit,artists,id_artists,release_date,danceability,energy,key,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,time_signature",
+  trackTransformedFileHeader:
+    "id,name,popularity,duration_ms,explicit,artists,id_artists,release_date,danceability,energy,key,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,time_signature,year,month,day",
+  artistFileHeader: "id,followers,genres,name,popularity",
+};
